c59600568c544afaf687bdceb3acc9ebbbcb71af
Jens D <jensdeweirdt@outlook.be>
2020-11-17 10:52:06 +0100

    Add Array count test

--- Cpp_DSs-and-Algos.Test/Arrays/DynamicArrayTest.cpp
+++ Cpp_DSs-and-Algos.Test/Arrays/DynamicArrayTest.cpp
@@ -15,5 +15,25 @@ namespace DynamicArrayTest
 			DynamicArray<int> *array = new DynamicArray<int>();
 			Assert::IsNotNull(array);
 		}
+
+		TEST_METHOD(TestDynamicArrayCountEquals8)
+		{
+			DynamicArray<int>* array = new DynamicArray<int>();
+			SetUpMainTestArray(array);
+			Assert::AreEqual(array->Count, 8);
+		}
+
+	private:
+		void SetUpMainTestArray(DynamicArray<int>* array) 
+		{
+			array->Push(1);
+			array->Push(2);
+			array->Push(4);
+			array->Push(5);
+			array->Push(6);
+			array->Push(8);
+			array->Push(9);
+			array->Push(10);
+		}
 	};
 }
